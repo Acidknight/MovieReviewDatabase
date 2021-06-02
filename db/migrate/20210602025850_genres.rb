f949b0a7ca8233968aba4c180bdbed7ef4b07e0d
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:05:03 -0400

    Add attributes to genres table

--- db/migrate/20210602025850_genres.rb
+++ db/migrate/20210602025850_genres.rb
@@ -1,4 +1,9 @@
 class Genres < ActiveRecord::Migration[6.1]
   def change
+    create_table "genres", force: :cascade do |t|
+      t.string "name"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
   end
 end
