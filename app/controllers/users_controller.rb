fb350b2dafaf174045d625d0ed3e59a3133a4090
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 01:06:44 -0400

    Add create method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            @error = @user.errors.full_messages.to_sentence
+            render :new
+        end
+    end
+
 end
