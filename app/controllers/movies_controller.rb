18631cba66d6634b48f53c6cad31409c96769d60
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 16:55:06 -0400

    Add index method to MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
 
     before_action :redirect_if_not_logged_in
 
+    def index
+        if params[:q]
+            @inventory_lists = InventoryList.search(params[:q])
+        else
+            @inventory_lists = InventoryList.alpha.all
+        end
+    end
+
 end
