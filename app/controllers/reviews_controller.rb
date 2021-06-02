d89ea1b559e2057ba266e81640ec6bfb25f64f95
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 18:00:49 -0400

    Add index method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,12 @@ class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
     before_action :current_user, only: [:edit, :update, :destroy]
 
+    def index
+        if params[:movie_id] && @movie = Movie.find_by_id(params[:movie_id])
+            @reviews = @movie.reviews
+        else
+            @reviews = Review.order_by_date
+        end
+    end
+
 end
