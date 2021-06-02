047a88c926f1b549eac057f0df8c991eea51f70a
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 17:22:04 -0400

    Change relationship logic

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
-    belongs_to :user
-    belongs_to :genre
     has_many :reviews
+    has_many :users, through: :reviews
     validates :title, :release_year, :summary, presence: true
 
     scope :alpha, -> { order('LOWER(name)') }
