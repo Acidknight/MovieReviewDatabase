d5b20bb8dc7d7cf4f303de2389bd4600c24a025c
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 17:17:02 -0400

    Add search function to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,11 @@ class Movie < ApplicationRecord
     has_many :reviews
     validates :title, :release_year, :summary, presence: true
 
+    scope :alpha, -> { order('LOWER(name)') }
+
+    def self.search(q)
+        InventoryList.where("name LIKE ?", "%#{q}%").alpha
+    end
+
 
 end
