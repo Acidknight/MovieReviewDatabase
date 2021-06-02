07d516da97593cf59e3b6555da9d5514f11dd258
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 17:20:34 -0400

    Change relationship logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    has_many :movies
     has_many :reviews
-    has_many :genres, through: :movies
+    has_many :movies, through: :reviews
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
