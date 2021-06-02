75a9689ababa63d756297d4f04951199e690f544
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:03:54 -0400

    Add attributes to the movies table

--- db/migrate/20210602025835_movies.rb
+++ db/migrate/20210602025835_movies.rb
@@ -1,4 +1,13 @@
 class Movies < ActiveRecord::Migration[6.1]
   def change
+    create_table "movies", force: :cascade do |t|
+      t.string "title"
+      t.string "release_year"
+      t.text "summary"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.integer "user_id"
+      t.integer "genre_id"
+    end
   end
 end
