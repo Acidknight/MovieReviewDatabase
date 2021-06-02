c334a5511e437c5cfce0f6953149560f697cee84
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:07:50 -0400

    Add attributes to users table

--- db/migrate/20210602025858_users.rb
+++ db/migrate/20210602025858_users.rb
@@ -1,4 +1,14 @@
 class Users < ActiveRecord::Migration[6.1]
   def change
+    create_table "users", force: :cascade do |t|
+      t.string "username"
+      t.string "email"
+      t.string "password_digest"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.string "uid"
+      t.string "image"
+      t.string "provider"
+    end  
   end
 end
