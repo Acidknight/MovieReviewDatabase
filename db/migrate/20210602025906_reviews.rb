6745fc31c27a9e286bb92ff9c23d44446eb67292
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:09:04 -0400

    Add attributes to reviews table

--- db/migrate/20210602025906_reviews.rb
+++ db/migrate/20210602025906_reviews.rb
@@ -1,4 +1,10 @@
 class Reviews < ActiveRecord::Migration[6.1]
   def change
+    create_table "reviews", force: :cascade do |t|
+      t.text "comment"
+      t.integer "movie_id"
+      t.integer "user_id"
+      t.integer "rating"
+    end
   end
 end
