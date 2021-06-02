9e24f6dce6caaaadcbf45b5ad5d06a016c67a3d7
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:15:01 -0400

    Add relationship logic to Genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,10 @@
 class Genre < ApplicationRecord
+    has_many :movies, inverse_of: :movie
+    has_many :users, through: :movies
+  
+  
+    def self.find_by_name(name)
+      Genre.find_by(name: name)
+    end
 
 end
