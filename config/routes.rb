20ec6fbf68c049986aaeb4cf0a3de768fed56ac5
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 17:12:47 -0400

    Add movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => "sessions#create"
   delete 'logout' => 'sessions#destroy'
+
+  resources :reviews
+  
+  resources :movies do
+    resources :reviews
+  end
 end
