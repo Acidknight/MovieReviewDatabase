66b082fed070127e40db9b2085fcb7a88775dc2b
Anthony Leon <Acidknights89@gmail.com>
2021-06-02 01:03:02 -0400

    Add create function for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,16 @@ class SessionsController < ApplicationController
         @user = User.find_by_id(params[:id])
     end
 
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user) 
+        else
+            flash[:error] = "Sorry, your username, email, or password were incorrect."
+            redirect_to '/login'
+        end
+    end
+
 end
