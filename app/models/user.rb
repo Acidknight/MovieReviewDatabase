54c5ae277a35683e9c5ab52d1a6e643e676ac4e3
Anthony Leon <Acidknights89@gmail.com>
2021-06-01 23:12:04 -0400

    Add relationship logic and secure password to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
 
+    has_secure_password
+
+    has_many :movies
+    has_many :reviews
+    has_many :genres, through: :movies
+
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
+
 end
